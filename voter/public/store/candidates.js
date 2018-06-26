6b7f2122eb67ac447fd6255b1193a6ed4a077d2c
S Romberg <SRomberg@Macbook-Pro.local>
2018-06-26 10:44:49 -0600

    server side added

--- voter/public/store/candidates.js
+++ voter/public/store/candidates.js
@@ -2,13 +2,17 @@
     name:"Mike Johnston",
     office:"governer",
     party:"democrat",
+    src:'https://www.mikejohnstonforcolorado.com/',
+    img: 'https://www.mikejohnstonforcolorado.com/static/aedf4497ab8fd6c0b46386a6a864979b-72357c896c2e80575e7796735d40658b-6f312.jpg',
     p1:"",
     p2:"",
-    p3:""
+    p3:"",
 }{
     name:"Cary Kennedy",
     office:"governer",
     party:"democrat",
+    src: 'https://carykennedyforgovernor.com/',
+    img: 'https://carykennedyforgovernor.com/wp-content/themes/ck/photos/ck-square-one-tile.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -16,6 +20,8 @@
     name:"Donna Lynne",
     office:"governer",
     party:"democrat",
+    src: 'http://lynneforcolorado.com/',
+    img: 'https://www.colorado.gov/pacific/sites/default/files/Donna%20Lynne-WEB2.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -23,6 +29,8 @@
     name:"Jarid Polis",
     office:"governer",
     party:"democrat",
+    src: 'https://polisforcolorado.com/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Jared_Polis_Official_2012.jpg/220px-Jared_Polis_Official_2012.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -30,6 +38,8 @@
     name:"Greg Lopez",
     office:"governer",
     party:"republican",
+    src: 'https://www.lopezforgovernor.com/',
+    img: 'https://static1.squarespace.com/static/593ec57f414fb5f67d830fe5/t/5953f810ff7c5021d74ad789/1498675278943/?format=500w',
     p1:"",
     p2:"",
     p3:""
@@ -37,6 +47,8 @@
     name:"Victor Mitchell",
     office:"governer",
     party:"republican",
+    src: 'http://www.vic4gov.com/',
+    img: 'https://i2.wp.com/www.denverpost.com/wp-content/uploads/2017/06/victor-mitchell-04132017-jl-02x.jpg?w=620&crop=0%2C0px%2C100%2C9999px&ssl=1',
     p1:"",
     p2:"",
     p3:""
@@ -44,6 +56,8 @@
     name:"Doug Robinson",
     office:"governer",
     party:"republican",
+    src: 'https://www.dougforcolorado.com/',
+    img: 'https://api.ballotpedia.org/v3/thumbnail/200/300/crop/best/professional_color_cropped.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -51,6 +65,8 @@
     name:"Walker Stapleton",
     office:"governer",
     party:"republican",
+    src: 'https://www.stapletonforcolorado.com/',
+    img: 'https://www.coloradopols.com/wp-content/uploads/2018/04/Screen-Shot-2018-04-10-at-12.36.32-PM.png',
     p1:"",
     p2:"",
     p3:""
@@ -60,6 +76,8 @@
     name:"Joe Salazar",
     office:"attorney general",
     party:"democrat",
+    src: 'https://salazarforcoag.com/',
+    img: 'https://api.ballotpedia.org/v3/thumbnail/200/300/crop/best/Joseph_Salazar.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -67,6 +85,8 @@
     name:"George Brauchler",
     office:"attorney general",
     party:"republican",
+    src: 'https://www.george2018.com/',
+    img: 'https://www.law.du.edu/sites/default/files/faculty-staff/George%20%20Brauchler/full-body/george-brauchler-fullbody.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -76,6 +96,8 @@
     name:"Jena Griswold",
     office:"secretary of state",
     party:"democrat",
+    src: 'https://www.jenaforcolorado.com/',
+    img: 'https://api.ballotpedia.org/v3/thumbnail/200/300/crop/best/Jena_Griswold.jpg'.
     p1:"",
     p2:"",
     p3:""
@@ -83,6 +105,8 @@
     name:"Wayne Williams",
     office:"secretary of state",
     party:"republican",
+    src: 'https://winwithwayne.org/',
+    img: 'https://api.ballotpedia.org/v3/thumbnail/200/300/crop/best/Wayne_Williams.jpg',
     p1:"",
     p2:"",
     p3:""
@@ -92,6 +116,8 @@
     name:"Debra Gustafson",
     office:"senate district 1",
     party:"democrat",
+    src: 'https://www.facebook.com/Debra-Gustafson-for-Senate-District-1-182859895700348/',
+    img: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-1/c0.0.323.323/31603204_182860379033633_3234936472192679936_n.jpg?_nc_cat=0&oh=9cb02dcb7856b9b5d3e06bb01a1f302d&oe=5BAF2BFF',
     p1:"",
     p2:"",
     p3:""
