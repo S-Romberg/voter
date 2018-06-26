c6f03d692378a676144b48d1cb9a12fa2355e37e
Dillon Means <dillon.j.means@gmail.com>
2018-06-26 10:10:48 -0600

    started candidate json

---
+++ voter/public/store/candidates.js
@@ -0,0 +1,379 @@
+{
+    name:"Mike Johnston",
+    office:"governer",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Cary Kennedy",
+    office:"governer",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Donna Lynne",
+    office:"governer",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Jarid Polis",
+    office:"governer",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Greg Lopez",
+    office:"governer",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Victor Mitchell",
+    office:"governer",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Doug Robinson",
+    office:"governer",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Walker Stapleton",
+    office:"governer",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}
+--------
+{
+    name:"Joe Salazar",
+    office:"attorney general",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"George Brauchler",
+    office:"attorney general",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}
+--------
+{
+    name:"Jena Griswold",
+    office:"secretary of state",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Wayne Williams",
+    office:"secretary of state",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}
+---------
+{
+    name:"Debra Gustafson",
+    office:"senate district 1",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Jerry Sonnenberg",
+    office:"senate district 1",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Beth Harz ",
+    office:"senate district 2",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Dennis Obduskey",
+    office:"senate district 2",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Dennis Hisey",
+    office:"senate district 2",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Stephanie Luck",
+    office:"senate district 2",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Leroy Garcia Jr.",
+    office:"senate district 3",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"John Pickerill",
+    office:"senate district 3",
+    party:"libertarian",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Kerry Donovan",
+    office:"senate district 5",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Olen Lund",
+    office:"senate district 5",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Guinn Unger Jr.",
+    office:"senate district 6",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Don Coram",
+    office:"senate district 6",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Chris Kennedy",
+    office:"senate district 7",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Ray Scott",
+    office:"senate district 7",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Gil Armendariz",
+    office:"senate district 9",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Pete Lee",
+    office:"senate district 11",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Pat McIntire",
+    office:"senate district 11",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Phil Kelley",
+    office:"senate district 13",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"John Cooke",
+    office:"senate district 13",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Rebecca Cranston",
+    office:"senate district 15",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Rob Woodward",
+    office:"senate district 15",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Tammy Story",
+    office:"senate district 16",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Tim Neville",
+    office:"senate district 16",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Jessie Danielson",
+    office:"senate district 20",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Christine Jensen",
+    office:"senate district 20",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Brittany Pettersen",
+    office:"senate district 22",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Tony Sanchez",
+    office:"senate district 22",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Faith Winter",
+    office:"senate district 24",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Beth Martinez Humenik",
+    office:"senate district 24",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Julia Varnell-Sarjeant",
+    office:"senate district 30",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Chris Holbert",
+    office:"senate district 30",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Steve Peterson",
+    office:"senate district 30",
+    party:"independent",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Robert Rodriguez",
+    office:"senate district 32",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Zach Neumann",
+    office:"senate district 32",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Hazel Gibson",
+    office:"senate district 32",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}
+{
+    name:"Mark Calonder",
+    office:"senate district 32",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Peter Smith",
+    office:"senate district 32",
+    party:"independant",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Julie Gonzales ",
+    office:"senate district 34",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Alan Kennedy-Shaffer",
+    office:"senate district 34",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Milo Schwab",
+    office:"senate district 34",
+    party:"democrat",
+    p1:"",
+    p2:"",
+    p3:""
+}{
+    name:"Gordon Alley",
+    office:"senate district 34",
+    party:"republican",
+    p1:"",
+    p2:"",
+    p3:""
+}
