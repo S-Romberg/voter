e7b48082b67d5d2823ab347348a106b1f1a492a0
Taylor <tayjed2010@gmail.com>
2018-06-27 13:46:29 -0600

    styles for fonts and candidates cards

--- voter-client/src/components/Welcome.jsx
+++ voter-client/src/components/Welcome.jsx
@@ -9,7 +9,7 @@ class Welcome extends Component {
           <img src='./assets/independent.jpg' alt='independent logo'/>
           <img  src='./assets/demo.png' alt='democrat logo'/>  
         </div>
-          <h1 className>Welcome To Voter</h1>
+          <h1 className='welcomeLogo'>Welcome To Voter</h1>
           <div className='paragraph'>
           <p>Voter is a platform that connects users with their congressional candidates.
              The goal of Voter is to make the voting process easier and more accessable by giving users the information they need to make educated choices about who to vote for in upcoming elections. By helping voters get the information they need we can encourage voters to get out and let their voices be heard! </p>
