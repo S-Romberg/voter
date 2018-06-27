3c9ee9a6e59bb7f7e3a57453229e6648aad32b81
Taylor <tayjed2010@gmail.com>
2018-06-27 15:42:00 -0600

    added map to resources page

--- voter-client/src/components/Resources.jsx
+++ voter-client/src/components/Resources.jsx
@@ -4,6 +4,9 @@ class Resources extends Component {
   render() {
     return (
       <div>
+        <div>
+          <iframe width="600" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/search?q=ballot&key=AIzaSyC9h7xh2v5hicF_trYIVmBDe9gnlP_b7JU" allowfullscreen></iframe>
+        </div> 
         <h2>Colorado Voter Resources</h2>
         <ul>
           <li><a href='https://www.dmv.org/co-colorado/voter-registration.php'>Register to Vote</a></li>
